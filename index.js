284f0145cf1bb113b4fe8e79dde4b3dced8b6cbe
Rudrik Patel <rudrik.patel@zymr.com>
2019-09-20 15:29:47 +0530

    Chapter1 -- Implemented the simple custom module -- imported the created custom module that will replace the replaceTemplate function.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 /////////////////////////////////////////////////////////////
 ///////////// FILES
 
@@ -22,21 +23,6 @@ const url = require('url');
 
 //////////////////////////////
 ///// SERVER
-
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
 // function replaceTemplate(temp, product) {
 //     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
 //     output = output.replace(/{%IMAGE%}/g, product.image);
@@ -63,7 +49,7 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
 
     const { query, pathname } = url.parse(req.url, true);
-    
+
     //OverView Page
     if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' })
