ad1acb12815042b98bc43aa74d60189fd1943a5c
Rudrik Patel <rudrik.patel@zymr.com>
2019-09-20 15:17:35 +0530

    Chapter1 -- Implemneted the ES6 destructing to fetch the data from the request url. -- Implemented the back button and the product details page view when click on the product.

--- index.js
+++ index.js
@@ -61,23 +61,25 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
 
-    const pathName = req.url;
-
+    const { query, pathname } = url.parse(req.url, true);
+    
     //OverView Page
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' })
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
 
         //Product Page
-    } else if (pathName === '/product') {
-        res.end('This is the product');
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
 
         //API
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
 
